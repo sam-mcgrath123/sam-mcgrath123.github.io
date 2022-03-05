869993d4179bc8f7c38d0858a4681b082d62dce3
SMcGrathISU <sgm@iastate.edu>
2022-03-05 10:14:37 -0600

    Sat Mar  5 10:14:26 CST 2022

---
+++ ghost-updater.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+date=$(date)
+git pull origin master
+rm -r docs
+mkdir docs
+cd docs
+echo "sam-mcgrath123.github.io/ghost-website/" > CNAME
+cd -
+ECTO1_SOURCE=http://localhost:2368 ECTO1_TARGET=https://sam-mcgrath123.github.io/ghost-website/ python3 ecto1.py
+cd docs
+#docker cp ghost:/var/lib/ghost/content/images/. content/images
+cd -
+grep -lR "srcset" docs/ | xargs sed -i 's/srcset/thisisbuggedatm/g'
+git add .
+git commit -m "$date"
+git config --global credential.helper store
+git push -u origin master
